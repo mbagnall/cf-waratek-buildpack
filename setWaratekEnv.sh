6bde3329aa248f1a55f5a4b4a33181c0bf9c5551
Martin Bagnall <mbagnall@waratek.com>
2020-07-04 09:56:50 +0100

    Launch app to be protected

--- setWaratekEnv.sh
+++ setWaratekEnv.sh
@@ -10,3 +10,8 @@ export WARATEK_ROOT=/home/vcap/app/waratek-secure
 # Extend JAVA_OPTS to use Waratek Secure
 echo "[INFO] Extend JAVA_OPTS to use Waratek Secure"
 export JAVA_OPTS="${JAVA_OPTS} -javaagent:${WARATEK_ROOT}/agent/waratek.jar -Dcom.waratek.WaratekProperties=${WARATEK_ROOT}/conf_1/waratek.properties"
+
+# Run application with Waratek
+echo "[INFO] Start Application with Waratek Secure"
+echo "[] Application start command is: ${APP_LAUNCH_CMD}"
+java -cp /home/vcap/app/. ${APP_LAUNCH_CMD}
