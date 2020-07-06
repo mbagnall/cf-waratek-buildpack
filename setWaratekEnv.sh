095e04905a9103ada475370b7fe09661ff30fd13
Martin Bagnall <mbagnall@waratek.com>
2020-07-06 11:17:34 +0100

    Adjustment to JAVA_OPTS

--- setWaratekEnv.sh
+++ setWaratekEnv.sh
@@ -9,11 +9,12 @@ export WARATEK_ROOT=/home/vcap/app/waratek-secure
 
 # Extend JAVA_OPTS to use Waratek Secure
 echo "[INFO] Extend JAVA_OPTS to use Waratek Secure"
-export JAVA_OPTS="${JAVA_OPTS} -javaagent:${WARATEK_ROOT}/agent/waratek.jar -Dcom.waratek.WaratekProperties=${WARATEK_ROOT}/conf_1/waratek.properties"
+#export JAVA_OPTS="${JAVA_OPTS} -javaagent:${WARATEK_ROOT}/agent/waratek.jar -Dcom.waratek.WaratekProperties=${WARATEK_ROOT}/conf_1/waratek.properties"
+export JAVA_OPTS="-javaagent:${WARATEK_ROOT}/agent/waratek.jar -Dcom.waratek.WaratekProperties=${WARATEK_ROOT}/conf_1/waratek.properties ${JAVA_OPTS}"
 
 # Run application with Waratek
 echo "[INFO] Start Application with Waratek Secure"
 echo "[INFO] Application start command is: ${APP_LAUNCH_CMD}"
 echo "[INFO] JAVA_OPTS to be used are: ${JAVA_OPTS}"
-#${APP_LAUNCH_CMD}
-export JAVA_OPTS="${JAVA_OPTS} -javaagent:${WARATEK_ROOT}/agent/waratek.jar -Dcom.waratek.WaratekProperties=${WARATEK_ROOT}/conf_1/waratek.properties" && ${APP_LAUNCH_CMD}
+${APP_LAUNCH_CMD}
+#export JAVA_OPTS="${JAVA_OPTS} -javaagent:${WARATEK_ROOT}/agent/waratek.jar -Dcom.waratek.WaratekProperties=${WARATEK_ROOT}/conf_1/waratek.properties" && ${APP_LAUNCH_CMD}
